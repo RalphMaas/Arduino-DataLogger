a833138ab50bcd022bafa0a51af86395cf53886e
Ralph Maas <rmaas@compete.nl>
2019-03-05 13:00:46 +0100

    clean comments

--- Arduino-DataLogger.ino
+++ Arduino-DataLogger.ino
@@ -10,22 +10,12 @@ Adafruit_INA219 sensor219; // Declare and instance of INA219
 #define I2C_ADDRESS 0x3C
 SSD1306AsciiAvrI2c oled;
 
-
-//#include "SdFat.h"
-//SdFat SD;
-
 unsigned long previousMillis = 0;
 unsigned long interval = 1000;
-//const int chipSelect = 10;
-//File LogFile;
 
 void setup(void) 
 {
    Serial.begin(9600);    
-  // Serial.println('SD');
-  // SD.begin(chipSelect);
-  // Serial.println('SD started');
-   
    oled.begin(&Adafruit128x64, I2C_ADDRESS);
    oled.setFont(Verdana12);
    oled.clear();    
@@ -50,7 +40,6 @@ void loop(void)
  unsigned long currentMillis = millis();
   if (currentMillis - previousMillis >= interval)
   {
-
     float shuntvoltage = 0;
     float busvoltage = 0;
     float voltage = 0; 
@@ -69,36 +58,24 @@ void loop(void)
         current = current + sensor219.getCurrent_mA();
     };
   
-    //voltage =  sensor219.getBusVoltage_V()/20;
     shuntvoltage = shuntvoltage /20;
     busvoltage = busvoltage /20;
-
-     //shuntvoltage = sensor219.getShuntVoltage_mV();
-    // busvoltage = sensor219.getBusVoltage_V();
-    // current = current + sensor219.getCurrent_mA();
-        
-    voltage = voltage + (busvoltage + (shuntvoltage/1000));
-    //voltage = busvoltage ;
+   
     current = sensor219.getCurrent_mA();
+    if (current <= 0 )
+    {
+        voltage = 0;
+    }
+    else
+    {
+        voltage = busvoltage + (shuntvoltage/1000);
+    }
+  
     power = voltage * (current/1000); // Calculate the Power
     energy = energy + voltage * current / 3600;
   
     displaydata(voltage, current, power, energy);
     displaySerial(shuntvoltage, busvoltage,voltage, current, power, energy);
-
-/*
-    LogFile = SD.open("LOG.csv", FILE_WRITE);
-    if (LogFile) {
-      Serial.print("Write to file"); 
-      LogFile.print(currentMillis);
-      LogFile.print(';');
-      LogFile.print(voltage);
-      LogFile.print(';');
-      LogFile.print('current');
-      
-      LogFile.close();
-    }
-*/    
  }
 }
 
@@ -121,19 +98,20 @@ void displaySerial(float& Us, float& Ub, float& U, float& A, float& P, float& E)
   
     Serial.print("Power:         "); 
     Serial.print(P); 
-    Serial.println(" mW");  
+    Serial.println(" W");  
 
     Serial.print("Energy:         "); 
     Serial.print(E); 
-    Serial.println(" mWh");  
+    Serial.println(" Wh");  
     Serial.println("");  
 }
 
 void displaydata(float& U, float& A, float& P, float& E) {
   showUnit(0,U,"U","V");
   showUnit(12,A,"I","mA");
-  showUnit(24,P,"P","mW");
-  showUnit(36,E,"E","mWh");
+  showUnit(24,P,"P","W");
+  showUnit(36,E,"E","Wh");
+
 }
 
 void showUnit(int line, float value,String symbol, String unit)
@@ -145,11 +123,11 @@ void showUnit(int line, float value,String symbol, String unit)
   oled.print(":");
 
   int x = 32;
-  if (value < 0)
-    x = x-4;
   oled.setCursor(x, line);
   oled.setFont(Verdana12_bold);
   oled.print(value);
+  oled.print("   ");
+
   
   oled.setCursor(85, line);
   oled.setFont(Verdana12);
